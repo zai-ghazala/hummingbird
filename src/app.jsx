1fd789eb441bf0831933ba744aac05bf0c35d417
Glitch (hopeisthethingwithfeathers) <none>
2021-08-30 13:27:53 +0000

    🎯🚧 Checkpoint ./src/styles/styles.css:4100075/5287 ./src/app.jsx:4100075/616 ./src/pages/home.jsx:4100075/42

--- src/app.jsx
+++ src/app.jsx
@@ -32,22 +32,6 @@ export default function Home() {
           <PageRouter />
         </div>
       </main>
-      {/* Footer links to Home and About, Link elements matched in router.jsx */}
-      <footer className="footer">
-        <div className="links">
-          <Link href="/">Home</Link>
-          <span className="divider">|</span>
-          <Link href="/about">About</Link>
-        </div>
-        <a
-          className="btn--remix"
-          target="_top"
-          href="https://glitch.com/edit/#!/remix/glitch-hello-react"
-        >
-          <img src="https://cdn.glitch.com/605e2a51-d45f-4d87-a285-9410ad350515%2FLogo_Color.svg?v=1618199565140" alt="" />
-          Remix on Glitch
-        </a>
-      </footer>
     </Router>
   );
 }
