3b5fb1f9d885d3e1271c59b2e6c40160a7fa78a3
Glitch (hopeisthethingwithfeathers) <none>
2021-08-30 13:52:45 +0000

    🎟🎠 Checkpoint ./src/components/random.jsx:4100075/3569 ./src/pages/home.jsx:4100075/199 ./src/styles/styles.css:4100075/43

--- src/components/random.jsx
+++ src/components/random.jsx
@@ -50,7 +50,11 @@ export class Random extends React.Component {
             {item.lines.map((line, i) => (
               <div key={"line-" + i}>
                 {line.split(" ").map((word, i) => (
-                  <DragDropContainer dragClone="true" targetKey="foo">
+                  <DragDropContainer
+                    dragClone="true"
+                    targetKey="foo"
+                    key={"drag-" + i}
+                  >
                     <span className="word" key={"word-" + i}>
                       {word}
                     </span>
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import { Link } from "wouter";
 import { Random } from "../components/random.jsx";
 
 export default function Home() {
@@ -7,12 +6,12 @@ export default function Home() {
     <>
       <div id="container">
         <img
-          id="img-top-right"
+          className="bird"
           src="https://cdn.glitch.com/ff50bc56-1555-4860-bbd4-f822f0922562%2Fhummingbird-right.png"
         />
         <Random />
         <img
-          id="img-bottom-left"
+          className="bird"
           src="https://cdn.glitch.com/ff50bc56-1555-4860-bbd4-f822f0922562%2Fhummingbird-left.png"
         />
       </div>
