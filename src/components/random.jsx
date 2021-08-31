dc35d20b7875929657394e6972cf1a8a4c7fae24
Glitch (hopeisthethingwithfeathers) <none>
2021-08-31 18:55:53 +0000

    🏆🚿 Checkpoint ./src/components/random.jsx:4100075/12151

--- src/components/random.jsx
+++ src/components/random.jsx
@@ -1,7 +1,5 @@
 import * as React from "react";
 import * as axios from "axios";
-import ReactDOM from "react-dom";
-import { v4 as uuid } from "uuid";
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
 const URL = "https://poetrydb.org/random,author/1;Dickinson";
@@ -40,22 +38,6 @@ export class Random extends React.Component {
     this.getPoem();
   }
 
-  getRenderItem = (items, className) => (provided, snapshot, rubric) => {
-  const item = items[rubric.source.index];
-  return (
-    <React.Fragment>
-      <li
-        {...provided.draggableProps}
-        {...provided.dragHandleProps}
-        ref={provided.innerRef}
-        style={provided.draggableProps.style}
-        className={snapshot.isDragging ? "dragging" : ""}
-      >
-        {item.label}
-      </li>
-    </React.Fragment>
-  );
-};
   render() {
     return (
       <>
@@ -63,21 +45,19 @@ export class Random extends React.Component {
           next
         </button>
 
-        <DragDropContext>
-          {this.state.poem.map((item, i) => (
-            <div key={"block-" + i}>
-              {item.lines.map((line, i) => (
-                <div key={"line-" + i}>
-                  {line.split(" ").map((word, i) => (
-                    <span className="word" key={"word-" + i}>
-                      {word}
-                    </span>
-                  ))}
-                </div>
-              ))}
-            </div>
-          ))}
-        </DragDropContext>
+        {this.state.poem.map((item, i) => (
+          <div key={"block-" + i}>
+            {item.lines.map((line, i) => (
+              <div key={"line-" + i}>
+                {line.split(" ").map((word, i) => (
+                  <span className="word" key={"word-" + i}>
+                    {word}
+                  </span>
+                ))}
+              </div>
+            ))}
+          </div>
+        ))}
         <div id="droppable"></div>
       </>
     );
