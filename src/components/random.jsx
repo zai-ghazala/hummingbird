06dd74d43df88995cf28dcaa10df9d21f629a17a
Glitch (hopeisthethingwithfeathers) <none>
2021-08-31 18:38:47 +0000

    🏨🏪 Checkpoint ./src/components/random.jsx:4100075/8040

--- src/components/random.jsx
+++ src/components/random.jsx
@@ -51,8 +51,7 @@ export class Random extends React.Component {
               {item.lines.map((line, i) => (
                 <div key={"line-" + i}>
                   {line.split(" ").map((word, i) => (
-          
-                    <span className="word" key={"word" + i}>
+                    <span className="word" key={"word-" + i}>
                       {word}
                     </span>
                   ))}
