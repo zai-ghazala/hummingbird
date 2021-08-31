362ff45803d11f10bfec891b044c534307421352
Glitch (hopeisthethingwithfeathers) <none>
2021-08-31 20:29:04 +0000

    🌈💾 Checkpoint ./src/components/draggable.jsx:4100075/11930 ./src/components/random.jsx:4100075/2995

--- src/components/draggable.jsx
+++ src/components/draggable.jsx
@@ -1,31 +1,37 @@
 import * as React from "react";
-import { useDrag } from 'react-dnd';
+import { useDrag } from "react-dnd";
 
-export default () => {
-    
-    const [, drag] = useDrag(() => ({ type: 'draggable'}));
-    return (
-        <AllowanceClass classes={classes} />
-    )
-}
+export const Box = () => {
+    const [, drag] = useDrag(() => ({ type: 'draggable' }));
+    return (<div ref={drag} style={style}>
+			Drag me
+		</div>);
+};
 
 export class Draggable extends React.Component {
-constructor(props) {
-    super();
-};
-  
-  
-  render(){
-    return ({this.props.poem.map((item, i) => (
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <>
+        <Drag />
+        {this.props.poem.map((item, i) => (
           <div key={"block-" + i}>
             {item.lines.map((line, i) => (
               <div key={"line-" + i}>
                 {line.split(" ").map((word, i) => (
-                  <span className="word" key={"word-" + i}>
+                  <span ref={drag} className="word" key={"word-" + i}>
                     {word}
                   </span>
                 ))}
               </div>
-            ))});
-};
-  )  }
+            ))}
+          </div>
+        ))}
+        ;
+      </>
+    );
+  }
+}
--- src/components/random.jsx
+++ src/components/random.jsx
@@ -4,12 +4,15 @@ import * as axios from "axios";
 import { DndProvider, useDrag } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 
+import { Draggable } from "../components/draggable.jsx";
+import { Droppable } from "../components/droppable.jsx";
+
 const URL = "https://poetrydb.org/random,author/1;Dickinson";
 
 
 export class Random extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.getPoem = this.getPoem.bind(this);
     this.state = {
       poem: []
@@ -48,7 +51,7 @@ export class Random extends React.Component {
         </button>
 
         <DndProvider backend={HTML5Backend}>
-        <Draggable/>
+    <Draggable/>
         <Droppable/>
         </DndProvider>
       </>
