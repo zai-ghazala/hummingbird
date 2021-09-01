b1eb5804b20f4f5579b7ccf15884fc8e9d0f3210
Glitch (hopeisthethingwithfeathers) <none>
2021-09-01 11:00:58 +0000

    👼🚊 Checkpoint ./src/components/random.jsx:4100075/11173

--- src/components/random.jsx
+++ src/components/random.jsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import axios from "axios";
 
 import { useState, useEffect } from "react";
 import { DndProvider, useDrag } from "react-dnd";
@@ -9,36 +8,26 @@ import Draggable from "../components/draggable.jsx";
 import Droppable from "../components/droppable.jsx";
 
 const Random = () => {
-  const [poem, setPoem] = useState([]);
-
-  const URL = "https://poetrydb.org/random,author/1;Dickinson";
+  const [poem, setPoem] = useState(null);
 
   useEffect(() => {
+    const URL = "https://poetrydb.org/random,author/1;Dickinson";
+    const fetchData = async () => {
+      try {
+        const response = await fetch(URL);
+        const json = await response.json();
+        console.log(json);
+        setPoem(json);
+      } catch (error) {
+        console.log("error", error);
+      }
+    };
+
     fetchData();
   }, []);
 
-  
-  useEffect(() => {
-   const fetchData = async () => {
-    const res = await axios(URL);
-    const data = await res.data;
-    setPoem({ poem: poem });
-    console.log(data);
-  };
-  fetchData();
- }, []);
-
-  const handleClick = e => {
-    e.preventDefault();
-    this.fetchData();
-  };
-
   return (
     <>
-      <button type="button" onClick={e => handleClick(e)}>
-        next
-      </button>
-
       <DndProvider backend={HTML5Backend}>
         <Draggable poem={poem} />
         <Droppable />
