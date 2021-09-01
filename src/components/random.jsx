3ec4e4ff660e3713f693f2af29c488896f55fac9
Glitch (hopeisthethingwithfeathers) <none>
2021-09-01 12:00:52 +0000

    🍖👘 Checkpoint ./src/components/random.jsx:4100075/3614

--- src/components/random.jsx
+++ src/components/random.jsx
@@ -1,31 +1,20 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 import { DndProvider, useDrag } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 
 import Draggable from "../components/draggable.jsx";
 import Droppable from "../components/droppable.jsx";
 
-
-function fetchData() {
-  return fetch('https://poetrydb.org/random,author/1;Dickinson')
-    .then(response => response.json())
-  .catch(error => console.log(error));
-}
-
 function Random() {
-  const [poem, setPoem] = useState({});
+  const [poem, setPoem] = useState([]);
 
-
- useEffect(() => {
-    fetchData.then(setPoem);
-    console.log("useEffect ran...");
-  }, []);
-
+  const URL = "https://poetrydb.org/random,author/1;Dickinson";
 
   return (
     <>
       <DndProvider backend={HTML5Backend}>
-        {<Draggable dataObject={poem} />}
+        {poem ? <Draggable poem={poem} /> : null}
         <Droppable />
       </DndProvider>
     </>
