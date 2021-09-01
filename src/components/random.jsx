42b94b34900f63a1484094e5a8d057156cc8451e
Glitch (hopeisthethingwithfeathers) <none>
2021-09-01 11:24:53 +0000

    👯😄 Checkpoint ./src/components/random.jsx:4100075/6023 ./src/components/draggable.jsx:4100075/14

--- src/components/random.jsx
+++ src/components/random.jsx
@@ -3,7 +3,7 @@ import * as React from "react";
 import { useState, useEffect } from "react";
 import { DndProvider, useDrag } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
-import axios from 'axios';
+import axios from "axios";
 
 import Draggable from "../components/draggable.jsx";
 import Droppable from "../components/droppable.jsx";
@@ -11,23 +11,21 @@ import Droppable from "../components/droppable.jsx";
 function Random() {
   const [poem, setPoem] = useState(null);
 
-
-  
-  useEffect(async () => {
-    
+  useEffect(() => {
     const URL = "https://poetrydb.org/random,author/1;Dickinson";
-    const result = await axios(
-      'https://hn.algolia.com/api/v1/search?query=redux',
-    );
- 
-    setData(result.data);
-  });
- 
+    const fetchData = async () => {
+      const result = await axios(URL);
+
+      setPoem(result.data);
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <>
       <DndProvider backend={HTML5Backend}>
-        <Draggable poem={poem} />
+        {poem && <Draggable poem={poem} />}
         <Droppable />
       </DndProvider>
     </>
