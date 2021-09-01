fcecd578e76516151d0f9a5954eee5a4cec0e8e3
Glitch (hopeisthethingwithfeathers) <none>
2021-09-01 20:09:34 +0000

    🤘⛱ Checkpoint ./src/components/draggable.jsx:4100075/8986

--- src/components/draggable.jsx
+++ src/components/draggable.jsx
@@ -1,39 +1,20 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { useDrag } from "react-dnd";
 import { ItemTypes } from "./types";
 
+
+    
 export const Draggable = props => {
-  const [word, setWord] = useState("");
+  
   const [, drag] = useDrag(() => ({ type: ItemTypes.WORD }));
+  
 
-  const showPoem = () => {
-    {
-      props.poem.map((item, i) => (
-        <div key={"block-" + i}>
-          {item.lines.map((line, i) => (
-            <div key={"line-" + i}>
-              {line.split(" ").map((word, i) => (
-              
-              
-                {const newRef = createRef();
-                LiRefs.push(newRef);
-                return (
-                
-                
-                <span ref={drag[i]} className="word" key={"word-" + i}>
-                {word}
+  return (
+    
+
+                <span ref={drag} className="word">
+                  {props.poems.lines.split(" ")}
                 </span>
-              ))}
-            </div>
-          ))}
-        </div>
-      ));
-    }
-  };
-
-  useEffect(() => {
-    showPoem();
-  }, []);
-
-  return <></>;
+    
+      );
 };
