3ee3584259dfe2cc4301cf01109c343a5fbe5dfb
Glitch (hopeisthethingwithfeathers) <none>
2021-09-01 10:24:51 +0000

    💁👮 Checkpoint ./src/components/draggable.jsx:4100075/386 ./src/components/random.jsx:4100075/2995

--- src/components/draggable.jsx
+++ src/components/draggable.jsx
@@ -1,25 +1,11 @@
 import * as React from "react";
-import { useState, useEffect } from "react";
-import * as axios from "axios";
 import { useDrag } from "react-dnd";
 
-function Draggable() {
-  const [poem, setPoem] = useState([]);
+function Draggable(props) {
 
-  useEffect(() => {
-    const fetchPoem = async () => {
-      const result = await axios(
-        "https://poetrydb.org/random,author/1;Dickinson"
-      );
-      setPoem(result.data);
-    };
-
-    fetchPoem();
-  }, []);
 
   const [, drag] = useDrag(() => ({ type: "draggable" }));
 
-  console.log(poem);
   return (
     <>
       {poem.map((item, i) => (
--- src/components/random.jsx
+++ src/components/random.jsx
@@ -1,4 +1,7 @@
 import * as React from "react";
+import axios from "axios";
+
+import { useState, useEffect } from "react";
 import { DndProvider, useDrag } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 
@@ -6,6 +9,24 @@ import Draggable from "../components/draggable.jsx";
 import Droppable from "../components/droppable.jsx";
 
 const Random = () => {
+  const [poem, setPoem] = useState([]);
+
+  const URL = "https://poetrydb.org/random,author/1;Dickinson";
+
+  useEffect(() => {
+    fetchPoem();
+  }, []);
+
+  const fetchPoem = () => {
+    axios
+      .get(URL)
+      .then(response => {
+        const lines = response.data.poem;
+        setPoem(lines);
+      })
+      .catch(error => console.error(`error: ${error}`));
+  };
+
   return (
     <>
       <button type="button" onClick={e => this.handleClick(e)}>
