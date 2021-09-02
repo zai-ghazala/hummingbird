bf5e7aec07d80d634be1ddfe21b6b0ae17bc8d30
Glitch (hopeisthethingwithfeathers) <none>
2021-09-02 19:58:20 +0000

    🏯🚧 Checkpoint ./src/pages/home.jsx:4100075/5567

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,9 +1,33 @@
 import * as React from "react";
 import Random from "../components/random.jsx";
-import { DndProvider } from "react-dnd-multi-backend";
-import { HTML5toTouch } from "rdndmb-html5-to-touch";
+import { HTML5Backend } from "react-dnd-html5-backend";
+import { TouchBackend } from "react-dnd-touch-backend";
 
-export default function Home() {
+import {
+  DndProvider,
+  TouchTransition,
+  MouseTransition,
+  createTransition
+} from "react-dnd-multi-backend";
+
+const HTML5toTouch = {
+  backends: [
+    {
+      id: "html5",
+      backend: HTML5Backend,
+      transition: MouseTransition
+    },
+    {
+      id: "touch",
+      backend: TouchBackend,
+      options: { enableMouseEvents: true },
+      preview: true,
+      transition: TouchTransition
+    }
+  ]
+};
+
+export Home = () => {
   return (
     <div id="container">
       <img
@@ -12,11 +36,11 @@ export default function Home() {
       />
       <DndProvider options={HTML5toTouch}>
         <Random />
-        <img
-          className="bird bird--left"
-          src="https://cdn.glitch.com/ff50bc56-1555-4860-bbd4-f822f0922562%2Fhummingbird-left.png"
-        />
       </DndProvider>
+      <img
+        className="bird bird--left"
+        src="https://cdn.glitch.com/ff50bc56-1555-4860-bbd4-f822f0922562%2Fhummingbird-left.png"
+      />
     </div>
   );
 }
