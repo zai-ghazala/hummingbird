ec7c12bcf64575a562998deb8a1db5da644cff69
Glitch (hopeisthethingwithfeathers) <none>
2021-09-02 17:39:58 +0000

    👗🏈 Checkpoint ./src/pages/home.jsx:4100075/911 ./src/app.jsx:4100075/104

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,18 +1,18 @@
 import Random from "../components/random.jsx";
-import { DndProvider } from "react-dnd-multi-backend";
-import HTML5toTouch from "react-dnd-multi-backend/dist/esm/HTML5toTouch";
 
 export default function Home() {
   return (
     <>
+      
       <div id="container">
         <img
           className="bird"
           src="https://cdn.glitch.com/ff50bc56-1555-4860-bbd4-f822f0922562%2Fhummingbird-right.png"
         />
-        <DndProvider options={HTML5toTouch}>
+        
+        
           <Random />
-        </DndProvider>
+        
         <img
           className="bird"
           src="https://cdn.glitch.com/ff50bc56-1555-4860-bbd4-f822f0922562%2Fhummingbird-left.png"
