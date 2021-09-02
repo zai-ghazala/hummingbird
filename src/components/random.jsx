01b1928efa63c6e9f352e9efc4bde90d588a3050
Glitch (hopeisthethingwithfeathers) <none>
2021-09-02 01:49:17 +0000

    🌵🗼 Checkpoint ./src/components/random.jsx:4100075/624

--- src/components/random.jsx
+++ src/components/random.jsx
@@ -5,23 +5,27 @@ import { Poem } from "./poem.jsx";
 import { Droppable } from "./droppable.jsx";
 
 function Random() {
-  const [poem, setPoem] = useState(null);
-  const [colour1, setColour1] = useState(null);
-  const [colour2, setColour2] = useState(null);
-  const [colour3, setColour3] = useState(null); 
   
-  useEffect(() => {
+  const buttonMap = {
+    button1: 'button1',
+    button2: 'button2',
+    button3: 'button3'
+}
+  const [poem, setPoem] = useState(null);
+ 
+  const [clicked, setClicked] = useState(); // Default button which you want to be clicked
+
+useEffect(() => {
     axios.get("https://poetrydb.org/random,author/1;Dickinson").then(res => {
       setPoem(res.data);
     });
   }, []);
 
-  const handleClick = poet => e => {
+  const handleClick = poet, button => e => {
     e.preventDefault();
     axios.get(`https://poetrydb.org/random,author/1;${poet}`).then(res => {
       setPoem(res.data);
-      setColour(!colour);
-      setColour(colour ? null : "pink");
+      
     });
   };
 
