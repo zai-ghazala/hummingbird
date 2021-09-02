f7d651cf98ae4090f9de176d54c515034b77506a
Glitch (hopeisthethingwithfeathers) <none>
2021-09-02 18:27:31 +0000

    🤑🏫 Checkpoint ./src/styles/styles.css:4100075/6972 ./src/components/random.jsx:4100075/4568 ./src/pages/home.jsx:4100075/15

--- src/components/random.jsx
+++ src/components/random.jsx
@@ -23,17 +23,21 @@ function Random() {
   return (
     <>
       <div className="buttons">
-      <button type="button" onClick={handleClick("Dickinson")}>emily dickinson</button>
-      <button type="button" onClick={handleClick("Rossetti")}>
-        christina rossetti
-      </button>
-      <button type="button" onClick={handleClick("Bronte")}>
-        emily bronte
-      </button>
-        
-        </div>
-      {poem && <Poem poem={poem} />}
-      <Droppable />
+        <button type="button" onClick={handleClick("Dickinson")}>
+          emily dickinson
+        </button>
+        <button type="button" onClick={handleClick("Rossetti")}>
+          christina rossetti
+        </button>
+        <button type="button" onClick={handleClick("Bronte")}>
+          emily bronte
+        </button>
+      </div>
+
+      <div className="poem">
+        <div>{poem && <Poem poem={poem} />}</div>
+        <Droppable />
+      </div>
     </>
   );
 }
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -3,7 +3,6 @@ import Random from "../components/random.jsx";
 
 export default function Home() {
   return (
-    <>
       <div id="container">
         <img
           className="bird"
@@ -15,6 +14,5 @@ export default function Home() {
           src="https://cdn.glitch.com/ff50bc56-1555-4860-bbd4-f822f0922562%2Fhummingbird-left.png"
         />
       </div>
-    </>
   );
 }
