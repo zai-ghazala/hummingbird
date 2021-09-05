8f8f786094b6b32fa149f34dc597f11d0794134f
Glitch (hopeisthethingwithfeathers) <none>
2021-09-05 22:11:44 +0000

    🛍💰 Checkpoint ./src/components/space.jsx:4100075/4213 ./src/components/random.jsx:4100075/4 ./src/styles/styles.css:4100075/91

--- src/components/space.jsx
+++ src/components/space.jsx
@@ -1,73 +1,31 @@
-import React, { useState } from "react";
-import { DropTarget, DragDropContainer } from "react-drag-drop-container";
-import { Word } from "./word.jsx";
-
-export const Space = props => {
-  let currentWord = props.currentWord;
-  
-  const [words, setWords] = useState([]);
-
-
-  const handleDrop = () => {
-        setWords([...words, currentWord]);
-    };
-  
-  
-  
-  return (
-    <DropTarget targetKey="drag" onHit={handleDrop}>
-      
-      {words.map((word, i) => {
-        return <Word key={"dropped_word" + i} word={word} />;
-      })}
-    </DropTarget>
-  );
-};
-
-
-
-
-
-import React from 'react';
-import { DragDropContainer, DropTarget } from '../../src';
-import { Word } from './word';
+import React from "react";
+import { DragDropContainer, DropTarget } from "react-drag-drop-container";
+import { Word } from "./word";
 
 export default class Space extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        words: []
-      };
-    }
-  
-    let currentWord = props.currentWord;
-  
-    handleDrop = (e) => {
-      let words = this.state.words.slice();
-      words.push({word: currentWord});
-      this.setState({words: words});
+  constructor(props) {
+    super(props);
+    this.state = {
+      words: []
     };
-  
-  
-  
-    render() {
-      return (
-       
-              <DropTarget
-                onHit={this.handleDrop}
-                targetKey="drag"
-              >
-                <div className="box">
-                  {this.state.words.map((word, i) => {
-                    return (
-                      <Word key={}"dropped_word" +}/>
-                      <>
-                    )
-                  })}
-                </div>
-              </DropTarget>
-            </DropTarget>
-        </div>
-      );
-    }
   }
+
+  handleDrop() {
+    let words = this.state.words.slice();
+    words.push({ word: this.props.currentWord });
+    this.setState({ words: words });
+    console.log(words);
+  }
+
+  render() {
+    return (
+      <DropTarget onHit={this.handleDrop} targetKey="drag">
+        {this.state.words.map((word, i) => {
+          return (
+            <Word key={"dropped_word" + i} word={this.props.currentWord} />
+          );
+        })}
+      </DropTarget>
+    );
+  }
+}
