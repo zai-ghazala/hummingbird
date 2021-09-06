6ada129e7036b47e7cc21fcab480c0c9515f0710
Glitch (hopeisthethingwithfeathers) <none>
2021-09-06 18:55:06 +0000

    🐀😻 Checkpoint ./src/components/space.jsx:4100075/9594

--- src/components/space.jsx
+++ src/components/space.jsx
@@ -4,35 +4,39 @@ import { Word } from "./word";
 
 export const Space = props => {
   const [composedPoem, setComposedPoem] = useState([]);
-  const [copySuccess, setCopySuccess] = useState([]);
+  const [copySuccess, setCopySuccess] = useState(false);
 
-  
-  const handleDrop = (e, currentWord) => {
-    e.preventDefault();
-    e.stopPropagation();
+  const handleDrop = currentWord => {
     setComposedPoem([...composedPoem, currentWord]);
   };
 
-  const handleClick = (e) => {
-    const el = this.input;
-    el.select()
-    document.execCommand("copy")
-    this.setState({copySuccess: true})
+  const handleClick = () => {
+    setCopySuccess(true);
   };
 
   return (
-    <div id="space">
-      <div className="footer2"></div><div><div>compose your poem here 📝</div><div><a onClick={() => handleClick}>{state.copySuccess ? '✨' : '⎘'}</a></div></div>
-      <DropTarget
-        handleDrag={props.handleDrag}
-        onHit={e => handleDrop(e, props.currentWord)}
-      >
-        <div className="enter">
-          {composedPoem.map((word, i) => {
-            return <Word key={"dropped_word" + i} word={word} />;
-          })}
+    <>
+      <div id="space">
+        <div className="footer2"></div>
+        <div>
+          <div>compose your poem here 📝</div>
+          <div>
+            <button type="button" onClick={() => {handleClick}>
+              {copySuccess ? "✨" : "⎘"}
+            </button>
+          </div>
         </div>
-      </DropTarget>
-    </div>
+        <DropTarget
+          handleDrag={props.handleDrag}
+          onHit={() => handleDrop(props.currentWord)}
+        >
+          <div className="enter">
+            {composedPoem.map((word, i) => {
+              return <Word key={"dropped_word" + i} word={word} />;
+            })}
+          </div>
+        </DropTarget>
+      </div>
+    </>
   );
 };
