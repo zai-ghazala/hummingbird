430b738ca5f4098453fc99499049fde683ce6678
Glitch (hopeisthethingwithfeathers) <none>
2021-09-06 18:27:53 +0000

    🎟🌄 Checkpoint ./src/styles/styles.css:4100075/194 ./src/components/space.jsx:4100075/1727

--- src/components/space.jsx
+++ src/components/space.jsx
@@ -6,27 +6,26 @@ export const Space = props => {
   const [composedPoem, setComposedPoem] = useState([]);
 
   const handleDrop = (e, currentWord) => {
+    e.preventDefault();
+    e.stopPropagation();
     setComposedPoem([...composedPoem, currentWord]);
   };
 
   console.log(composedPoem);
 
   return (
-    
-      <div id="space">
-      
-         <div className="footer2">compose your poem here 📝</div>
-    <DropTarget
-      handleDrag={props.handleDrag}
-      onHit={e => handleDrop(e, props.currentWord)}
-    >
-      <div className="enter">
-        {composedPoem.map((word, i) => {
-          return <Word key={"dropped_word" + i} word={word} />;
-        })}
-      </div>
-    </DropTarget>
-    
+    <div id="space">
+      <div className="footer2">compose your poem here 📝</div>
+      <DropTarget
+        handleDrag={props.handleDrag}
+        onHit={e => handleDrop(e, props.currentWord)}
+      >
+        <div className="enter">
+          {composedPoem.map((word, i) => {
+            return <Word key={"dropped_word" + i} word={word} />;
+          })}
+        </div>
+      </DropTarget>
     </div>
   );
 };
