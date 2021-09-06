389d738ba8d07608084cd8dde71bd8d56dc65873
Glitch (hopeisthethingwithfeathers) <none>
2021-09-06 07:50:56 +0000

    😬🍇 Checkpoint ./src/components/space.jsx:4100075/4 ./src/styles/styles.css:4100075/21081

--- src/components/space.jsx
+++ src/components/space.jsx
@@ -2,12 +2,12 @@ import React, { useState } from "react";
 import { DropTarget } from "react-drag-drop-container";
 import { Word } from "./word";
 
-export const Space = props => {
+export const Space = (props) => {
   const [composedPoem, setComposedPoem] = useState([]);
 
   console.log("out", props.currentWord);
 
-  const handleDrop = currentWord => {
+  const handleDrop = (currentWord) => {
     setComposedPoem([...composedPoem, currentWord]);
     console.log("handleDrop", props.currentWord, currentWord);
   };
