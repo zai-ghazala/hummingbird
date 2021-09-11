760e3248cf21b021816604ee79fe9870807cbbb8
Glitch (hopeisthethingwithfeathers) <none>
2021-09-11 14:09:37 +0000

    🚀🌊 Checkpoint ./index.html:4100075/2910 ./src/seo.json:4100075/14 ./src/pages/home.jsx:4100075/1185

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -4,23 +4,21 @@ import Random from "../components/random";
 export default function Home() {
   return (
     <>
-    <div id="container">
-      <img
-        className="bird bird--right"
-        src="https://cdn.glitch.com/ff50bc56-1555-4860-bbd4-f822f0922562%2Fhummingbird-right.png"
-      />
+      <div id="container">
+        <img
+          className="bird bird--right"
+          src="https://cdn.glitch.com/ff50bc56-1555-4860-bbd4-f822f0922562%2Fhummingbird-right.png"
+        />
 
-      <Random />
+        <Random />
 
-      <img
-        className="bird bird--left"
-        src="https://cdn.glitch.com/ff50bc56-1555-4860-bbd4-f822f0922562%2Fhummingbird-left.png"
-      />
-      
-         <div className="footer">made with 💖 by zainab ismail</div>
-   
-    </div>
-   
+        <img
+          className="bird bird--left"
+          src="https://cdn.glitch.com/ff50bc56-1555-4860-bbd4-f822f0922562%2Fhummingbird-left.png"
+        />
+
+        <div className="footer">made with 💖 by zainab ismail</div>
+      </div>
     </>
   );
 }
