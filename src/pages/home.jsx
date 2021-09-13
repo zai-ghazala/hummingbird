35c8d08538d3852e46c37e832880a4f37b980d9c
zai-i <zainab@wonderbly.com>
2021-09-13 09:48:38 +0100

    images now on git

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,5 +1,7 @@
 import * as React from "react";
 import Random from "../components/random";
+import right from "../assets/hummingbird--right.png";
+import left from "../assets/hummingbird--left.png";
 
 export default function Home() {
   return (
@@ -7,14 +9,14 @@ export default function Home() {
       <div id="container">
         <img
           className="bird bird--right"
-          src="https://cdn.glitch.com/ff50bc56-1555-4860-bbd4-f822f0922562%2Fhummingbird-right.png"
+          src={right}
         />
 
         <Random />
 
         <img
           className="bird bird--left"
-          src="https://cdn.glitch.com/ff50bc56-1555-4860-bbd4-f822f0922562%2Fhummingbird-left.png"
+          src={left}
         />
 
         <div className="footer">made with 💖 by zainab ismail</div>
