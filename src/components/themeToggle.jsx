bb28377cc0a2ecd3df24532648b81c1763d412bc
zai-i <zainab@wonderbly.com>
2023-02-01 20:38:57 +0000

    Add hi contrast palette

--- src/components/themeToggle.jsx
+++ src/components/themeToggle.jsx
@@ -23,7 +23,7 @@ export const ThemeToggle = () => {
   },[]);
 
   const nextTheme = () => {
-    const allThemes = ['darkPalette', 'nudePalette', 'tealPalette', 'glassPalette', 'plainPalette', 'defaultPalette']
+    const allThemes = ['darkPalette', 'nudePalette', 'tealPalette', 'glassPalette', 'plainPalette', 'contrastPalette',  'defaultPalette']
 
     setCount(count + 1);
 
